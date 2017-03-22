7d22074f3d3fae644af8575b5556a4d874c8d5a6
flaxmans <sam.flaxman@gmail.com>
2017-03-22 09:56:06 -0600

    started an example R script called UniqueCounts.R

---
+++ UniqueCounts.R
@@ -0,0 +1,9 @@
+# function that counts numbers of unique entries in each column/variable of a data frame
+# note: this is a test file for the ForkAndPullDemo repo
+
+UniqueCounts <- function( mydf ){
+  nvars <- dim(mydf)[2]
+  uniqueNums <- matrix(data = NA, nrow = nvars, ncol = 2)
+  
+  return( uniqueNums )
+}
