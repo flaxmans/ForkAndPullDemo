f4aa670e0fbf6dbcc9a429909b67ef3b5a5f39d1
flaxplus <sam.flaxman@protonmail.com>
2017-03-22 10:24:10 -0600

    added lines and changes to UniqueCounts to make the returned data object better

--- UniqueCounts.R
+++ UniqueCounts.R
@@ -1,9 +1,15 @@
 # function that counts numbers of unique entries in each column/variable of a data frame
 # note: this is a test file for the ForkAndPullDemo repo
 
+# looks like flaxmans didn't finish this function yet
+# I have some ideas to add; I think a data frame
+# would be better than a matrix
 UniqueCounts <- function( mydf ){
   nvars <- dim(mydf)[2]
-  uniqueNums <- matrix(data = NA, nrow = nvars, ncol = 2)
+  myvarnames <- names(mydf)
+  counts <- vector(mode = "numeric", length = nvars) 
+  uniqueNums <- data.frame(myvarnames,counts)
+  
   
   return( uniqueNums )
 }
